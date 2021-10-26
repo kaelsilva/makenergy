4c89bf971cbeb423c7420d0411b309b42385536b
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-10-26 09:41:58 -0300

    chore: add client management page to app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Chart from './pages/Chart';
+import ManageClients from './pages/ManageClients';
 
 const App: React.FC = () => {
-  return <Chart />;
+  return (
+    <>
+      {/* <Chart /> */}
+      <ManageClients />
+    </>
+  );
 };
 
 export default App;
