b6c77a769b4aed1d6c7c8faaa773f508da478f69
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-11-01 15:18:36 -0300

    chore: add styles to client management page

--- web/src/pages/ManageClients/styles.ts
+++ web/src/pages/ManageClients/styles.ts
@@ -30,10 +30,17 @@ export const List = styled(MList)`
   align-content: space-between;
 `;
 
-export const ListItemText = styled(MListItemText)`
+export const ListItemContainer = styled.div`
+  display: flex;
+  flex: 1;
+  height: 50px;
   border-color: #efefef;
   border-width: 2px;
   border-style: solid;
+`;
+
+export const ListItemText = styled(MListItemText)`
+  background-color: #fefefe;
 `;
 
 export const DeleteButton = styled(MButton)`
@@ -72,6 +79,41 @@ export const EditButton = styled(MButton)`
   }
 `;
 
+export const CancelButton = styled(MButton)`
+  && {
+    background-color: #ccc;
+    color: #000;
+    border-radius: 5px;
+
+    &:hover {
+      background-color: ${shade(0.2, '#ccc')};
+    }
+  }
+`;
+
+export const NewButton = styled(MButton)`
+  && {
+    background-color: #fff;
+    color: #4bb543;
+    width: 60px;
+    min-width: 60px;
+    height: 60px;
+    min-height: 60px;
+    border-radius: 50%;
+
+    &:hover {
+      background-color: ${shade(0.2, '#fff')};
+    }
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: fit-content;
+  align-content: flex-end;
+`;
+
 export const CreationContainer = styled.form`
   && {
     background-color: #fff;
@@ -84,6 +126,28 @@ export const CreationContainer = styled.form`
     justify-content: space-between;
     width: 400px;
     padding: 20px;
+
+    border-style: solid;
+    border-width: 2px;
+    border-color: #efefef;
+  }
+`;
+
+export const EditContainer = styled.form`
+  && {
+    background-color: #fff;
+    position: absolute;
+    z-index: 1;
+    top: 100px;
+    display: flex;
+    flex-direction: column;
+    flex-basis: auto;
+    flex-grow: 1;
+    overflow-y: auto;
+    height: 400px;
+    justify-content: space-between;
+    width: 400px;
+    padding: 50px;
 
     border-style: solid;
     border-width: 2px;
