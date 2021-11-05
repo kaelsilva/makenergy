eb285a5ae6da13070f5caf3a723c593b6d8e9a0d
kaelsilva <kaelsilvaaraujo@gmail.com>
2021-11-04 21:31:02 -0300

    chore: add routes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import ManageClients from '../pages/ManageClients';
 import Chart from '../pages/Chart';
+import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
 
 const Routes: React.FC = () => (
   <Switch>
+    <Route path={`${process.env.PUBLIC_URL}/`} exact component={Login} />
+    <Route path={`${process.env.PUBLIC_URL}/signup`} component={SignUp} />
     <Route
-      path={`${process.env.PUBLIC_URL}/`}
-      exact
+      path={`${process.env.PUBLIC_URL}/clients`}
       component={ManageClients}
+      isPrivate
     />
-    <Route path={`${process.env.PUBLIC_URL}/chart`} exact component={Chart} />
+    <Route
+      path={`${process.env.PUBLIC_URL}/chart`}
+      component={Chart}
+      isPrivate
+    />
   </Switch>
 );
 
